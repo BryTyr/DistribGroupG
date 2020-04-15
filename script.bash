bcefaa12816a4583298a766deb84655b43b456de
farrej10 <farrej10@tcd.ie>
2020-04-15 19:02:29 +0100

    logging of activity of nodes

---
+++ script.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+gnome-terminal --geometry=30x10+20+25 -- bash -c  "python3 /home/james/Documents/Distro/SecondOne/DistribGroupG/MainRunFile12345.py; exec bash"
+gnome-terminal --geometry=30x10+520+25 -- bash -c  "python3 /home/james/Documents/Distro/SecondOne/DistribGroupG/MainRunFile11111111.py; exec bash"
+gnome-terminal --geometry=30x10+1020+25 -- bash -c  "python3 /home/james/Documents/Distro/SecondOne/DistribGroupG/MainRunFile11111112.py; exec bash"
+gnome-terminal --geometry=30x10+1520+25 -- bash -c  "python3 /home/james/Documents/Distro/SecondOne/DistribGroupG/MainRunFile11111113.py; exec bash"
